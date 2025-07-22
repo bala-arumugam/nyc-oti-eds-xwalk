e6392845c59f85eb68ccd00b5d9d9b87fa99d9bd
DOITT\juduran <juduran@oti.nyc.gov>
2025-07-22 17:12:12 -0400

    body-container

--- blocks/body/body.js
+++ blocks/body/body.js
@@ -1,3 +1,8 @@
 export default function decorate(doc){
+  const firstChild = doc.firstElementChild;
+  const secondChild = firstChild.firstElementChild;
+  
+  firstChild.className = "oit-fluid-container"
+  secondChild.className = "oit-row"
   debugger
 }
