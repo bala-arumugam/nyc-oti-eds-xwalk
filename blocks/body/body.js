28690e65c8d3f0e740bcabf10ef6aee32bdef9eb
Ippolito <mippolito@oti.nyc.gov>
2025-07-29 14:12:23 -0400

    updated hero based on lint clean up

--- blocks/body/body.js
+++ blocks/body/body.js
@@ -1,8 +1,7 @@
-export default function decorateBlock(doc){
+export default function decorateBlock(doc) {
   const firstChild = doc.firstElementChild;
   const secondChild = firstChild.firstElementChild;
-  
-  firstChild.className = "oit-fluid-container"
-  secondChild.className = "oit-row"
-  debugger
+
+  firstChild.className = 'oit-fluid-container';
+  secondChild.className = 'oit-row';
 }
