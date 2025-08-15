331b99be1934491a217e5bf33129c50e14468894
DOITT\juduran <juduran@oti.nyc.gov>
2025-08-15 18:50:47 -0400

    Add CTA button banner styles and structure with SVG icon

--- blocks/cta-button-banner/cta-button-banner.js
+++ blocks/cta-button-banner/cta-button-banner.js
@@ -1,24 +1,26 @@
-import { createElement } from "../../scripts/util";
+import { createElement } from "../../scripts/util.js";
 
 export default function decorate(doc) {
   // Get Data
   const title = doc.children[0].textContent.trim();
   const description = doc.children[1].textContent.trim();
-  const button = doc.children[2].doc.children[2].firstElementChild;
-    doc.innerHtml = ``;
+  const button = doc.children[2]?.firstElementChild;
+  // Clear the contents of the doc element
+  while (doc.firstChild) {
+    doc.removeChild(doc.firstChild);
+  }
 
   const template =   `
     <div class="cta-banner-button-text">
-      <div class="cta-banner-button-text-title">${title}</div>
+      <h5 class="cta-banner-button-text-title">${title}</h5>
       <div class="cta-banner-button-text-description">${description}</div>
     </div>
     <div class="cta-banner-button-action">
-      ${button}
+      ${button ? button.outerHTML : ''}
     </div>
   `;
 
-  const div = createElement("div", {props:{className:"cta-banner-button"}})
+  const div = createElement("div")
   div.innerHTML = template;
-
   doc.appendChild(div);
 }
