c7816a8535688cf6234b0c9b1762961a25c8cef0
DOITT\juduran <juduran@oti.nyc.gov>
2025-08-15 16:53:07 -0400

    Refactor CTA button banner component to improve data handling and structure

--- blocks/cta-button-banner/cta-button-banner.js
+++ blocks/cta-button-banner/cta-button-banner.js
@@ -1,3 +1,28 @@
-export default function decorate() {
+import { createElement, appendChild } from "../../scripts/util";
+
+
+export default function decorate(doc) {
+  // Get Data
+  const title = doc.children[0].textContent.trim();
+  const description = doc.children[1].textContent.trim();
+  const button = doc.children[2].doc.children[2].firstElementChild;
+    doc.innerHtml = ``;
+
+  const template =   `
+    <div class="cta-banner-button-text">
+      <div class="cta-banner-button-text-title">${title}</div>
+      <div class="cta-banner-button-text-description">${description}</div>
+    </div>
+    <div class="cta-banner-button-action">
+      ${button}
+    </div>
+  `;
+
+  const div = createElement("div", {props:{className:"cta-banner-button"}})
+  div.innerHTML = template;
+
+
+
+  doc.appendChild(div)
 
 }
