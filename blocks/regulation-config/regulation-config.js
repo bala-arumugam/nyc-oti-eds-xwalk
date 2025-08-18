e0828a3000224bc448d2656dde1900c890f9242b
Ippolito <mippolito@oti.nyc.gov>
2025-08-18 11:52:04 -0400

    updated to add required fields for page properties as well as updating the hero based on title from page properties

--- blocks/regulation-config/regulation-config.js
+++ blocks/regulation-config/regulation-config.js
@@ -466,9 +466,9 @@ export default async function decorate(doc) {
 
   // Get the page metadata for jcr:title instead of using the text from pElement
   // This will use the "Page Name" from the page metadata
-  const pageTitle = document.querySelector('meta[property="page-property-name"]')?.content ||
-                    document.querySelector('meta[property="title"]')?.content ||
-                    document.title;
+  const pageTitle = document.querySelector('meta[property="page-property-name"]')?.content
+                    || document.querySelector('meta[property="title"]')?.content
+                    || document.title;
   h1.textContent = pageTitle;
   h1.className = 'page-container regulation-page-hero-text';
   div.appendChild(h1);
