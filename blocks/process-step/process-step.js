3140f1d60cd783c9ef8ff9e28faf0868ac53a0db
DOITT\juduran <juduran@oti.nyc.gov>
2025-08-20 17:21:41 -0400

    refactor: simplify step element extraction logic in process-step.js

--- blocks/process-step/process-step.js
+++ blocks/process-step/process-step.js
@@ -27,7 +27,12 @@ export default async function decorate(block) {
   }
 
   template.appendChild(newTitle);
-  template.appendChild(stepElement.children[0]);
+  const step = Array.isArray(stepElement) && stepElement.length > 0 
+    ? stepElement[0].children[0] 
+    : stepElement && stepElement.children 
+     ? stepElement.children[0] 
+     : stepElement;
+  template.appendChild(step);
 
   // Clear the content of doc
   while (block.firstChild) {
