02ad98d569e659c8d08536aeb64da36439416819
DOITT\juduran <juduran@oti.nyc.gov>
2025-09-04 09:54:33 -0400

    Update contact info structure and styles
    - Set fax field as not required in JSON schema
    - Adjust CSS for paragraph margins and link icon display
    - Improve address formatting in JavaScript
    - Integrate icon decoration functionality

--- blocks/contact-info/contact-info.js
+++ blocks/contact-info/contact-info.js
@@ -1,3 +1,4 @@
+import { decorateIcons } from '../../scripts/aem.js';
 import getContentFragment from '../../scripts/regulation-page-labels.js';
 import { createElement } from '../../scripts/util.js';
 
@@ -47,7 +48,7 @@ export default function decorate(block) {
     ${m.agencyName ? `<h5>${m.agencyName}</h5>` : ''}
     ${m.officeDepartment ? `<p>${m.officeDepartment}</p>` : ''}
     ${`<p>${m.streetOne}`} ${m.streetTwo ? `, ${m.streetTwo}</p>` : '</p>'}
-    <p>${m.city} ${m.state} ${m.zipCode}</p>
+    <p>${m.city}, ${m.state}, ${m.zipCode}</p>
     ${m.email ? `<h6>Email:</h6><p><a href='mailto:${m.email}'>${m.email}</a></p>` : ''}
     ${m.website ? `<h6>Website:</h6><p><a href='${m.website}' target='_blank' rel='noopener noreferrer'>${m.website}<span class='icon icon-outlink'></span></a></p>` : ''}
     ${m.phone ? `<h6>Phone:</h6><p>${formatPhoneNumber(m.phone)}</p>` : ''}
@@ -70,18 +71,6 @@ export default function decorate(block) {
 
     // Create content container
     const contentContainer = document.createElement('div');
-    contentContainer.className = 'contact-info-content';
-
-    // Transfer the content
-    if (contentDiv.innerHTML.trim()) {
-      contentContainer.innerHTML = contentDiv.innerHTML;
-    } else {
-      // If no content, provide a placeholder or default message
-      const placeholderText = document.createElement('p');
-      placeholderText.className = 'contact-info-placeholder';
-      placeholderText.textContent = 'Contact information will be displayed here.';
-      contentContainer.appendChild(placeholderText);
-    }
 
     container.appendChild(contentContainer);
   }
@@ -90,6 +79,8 @@ export default function decorate(block) {
   div.innerHTML = template;
 
   container.appendChild(div);
+
+  decorateIcons(container)
 
   // Append the fully built container to the block
   block.appendChild(container);
